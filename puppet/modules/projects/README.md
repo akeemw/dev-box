936d71c992368331faeae829836290aa4cf0d0dd
Akeem Williams <akeem@oho.com>
2014-04-24 06:55:08 -0400

    Moving information over to a readme file for the custom projects module

---
+++ puppet/modules/projects/README.md
@@ -0,0 +1,6 @@
+# Puppet module: projects
+
+* Original: https://github.com/mikebell/drupaldev-apache
+* Added Flavor: https://github.com/drupalboxes/drupal-puppet
+
+Made by Akeem Williams
--- puppet/modules/projects/manifests/init.pp
+++ puppet/modules/projects/manifests/init.pp
@@ -1,6 +1,3 @@
-# Original: https://github.com/mikebell/drupaldev-apache
-# Added Flavor: https://github.com/drupalboxes/drupal-puppet
-
 class projects {
   file { [ "/srv/web/"  ]:
     ensure => directory

f26d334f41dfeb304fbc845acb3af00c23d8d1a1
Akeem Williams <akeem@oho.com>
2014-05-06 22:54:26 -0400

    Commenting out the example D7 installation

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,5 +1,6 @@
 /* Install Drupal 7 from GIT */
 
+/*
 projects::create { 'd7':
   git_url => 'http://git.drupal.org/project/drupal.git',
   git_branch => '7.x',
@@ -7,3 +8,4 @@ projects::create { 'd7':
     'securepages_enable' => 0,
   },
 }
+*/

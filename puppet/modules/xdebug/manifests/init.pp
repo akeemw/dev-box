4e9d5783ffed4129d69a8701134e587669c330c2
Akeem Williams <akeemw@gmail.com>
2015-01-05 01:21:58 -0500

    Allow control over the location of the XDebug.so from within the YML file

--- puppet/modules/xdebug/manifests/init.pp
+++ puppet/modules/xdebug/manifests/init.pp
@@ -1,18 +1,17 @@
 class xdebug (
-  $service              = "httpd",
-  $ini_file_path        = $xdebug::params::ini_file_path,
-  $default_enable       = $xdebug::params::default_enable,
-  $remote_enable        = $xdebug::params::remote_enable,
-  $remote_handler       = $xdebug::params::remote_handler,
-  $remote_host          = $xdebug::params::remote_host,
-  $remote_port          = $xdebug::params::remote_port,
-  $remote_autostart     = $xdebug::params::remote_autostart,
-  $remote_connect_back  = $xdebug::params::remote_connect_back,
-  $remote_log           = $xdebug::params::remote_log,
-  $idekey               = $xdebug::params::idekey,
-) inherits xdebug::params {
+  $service               = "httpd",
+  $ini_file_path         = $xdebug::params::ini_file_path,
+  $default_enable        = $xdebug::params::default_enable,
+  $remote_enable         = $xdebug::params::remote_enable,
+  $remote_handler        = $xdebug::params::remote_handler,
+  $remote_host           = $xdebug::params::remote_host,
+  $remote_port           = $xdebug::params::remote_port,
+  $remote_autostart      = $xdebug::params::remote_autostart,
+  $remote_connect_back   = $xdebug::params::remote_connect_back,
+  $remote_log            = $xdebug::params::remote_log,
+  $idekey                = $xdebug::params::idekey,
   $zend_extension_module = $xdebug::params::zend_extension_module
-
+) inherits xdebug::params {
   package { "$xdebug::params::package":
     ensure => "installed",
     require => Package[$xdebug::params::php],
